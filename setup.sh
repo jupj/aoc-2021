9876b7102eded24acd739c0badecbc428a3430ba
Johan Jansson <johan.jansson@iki.fi>
2021-12-12 13:53:12 +0200

    Improve setup.sh
    - merge getdesc.sh into setup.sh
    - make more idempotent

--- setup.sh
+++ setup.sh
@@ -3,23 +3,37 @@
 daydir=$(date +"day%d")
 prevdir=$(date -d yesterday +"day%d")
 
-if [ -d "$daydir" ]; then
-    >&2 echo "Error: $daydir exists already"
-    exit 1
-fi
-
-mkdir "$daydir"
-
-if [ -d "$prevdir" ]; then
-    # Copy files
-    cp "$prevdir"/* "$daydir"
-
-    # Rename filename to current day
-    mv "$daydir/$prevdir.c" "$daydir/$daydir.c"
-    sed -i "s/$prevdir/$daydir/g" "$daydir/Makefile"
-fi
-
 daynr=$(date +"%d" | sed "s/^0//") # Strip prefix 0 from day number
-curl -sS --cookie .cookie "https://adventofcode.com/2021/day/$daynr/input" > "$daydir/input.txt"
 
-./getdesc.sh "$daynr" > "$daydir/description.md"
+if ! [ -d "$daydir" ]; then
+    # Initialize only once
+    mkdir "$daydir"
+
+    if [ -d "$prevdir" ]; then
+        # Copy files
+        cp "$prevdir"/* "$daydir"
+
+        # Rename filename to current day
+        mv "$daydir/$prevdir.c" "$daydir/$daydir.c"
+        sed -i "s/$prevdir/$daydir/g" "$daydir/Makefile"
+
+        # Clear test_input.txt
+        rm "$daydir/test_input.txt"
+        touch "$daydir/test_input.txt"
+    fi
+
+    # Write test_expected.txt
+    echo "Part one: _" > "$daydir/test_expected.txt"
+    echo "Part two: _" >> "$daydir/test_expected.txt"
+
+    # Write input.txt
+    curl -sS --cookie .cookie "https://adventofcode.com/2021/day/$daynr/input" > "$daydir/input.txt"
+fi
+
+# Update description.md
+curl -sS --cookie .cookie "https://adventofcode.com/2021/day/$daynr" | \
+    pandoc -f html -t markdown | \
+    awk '
+        inmain          { print }
+        /role="main"/   { inmain = 1 }
+    ' > "$daydir/description.md"
